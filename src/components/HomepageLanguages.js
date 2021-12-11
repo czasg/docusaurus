2ee00e714b034afec0ed44ce5733f0b823323810
czasg <czasg0.0>
2021-12-11 13:59:01 +0800

    feat: update

---
+++ src/components/HomepageLanguages.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageLanguages.module.css';
+
+export default function HomepageLanguages() {
+    return (
+        <section className={clsx('hero hero--dark', styles.features)}>
+            <div className="container">
+                <div className="row">
+                    halo
+                </div>
+            </div>
+        </section>
+    );
+}
