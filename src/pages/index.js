329410abba50e5c92375fbe645c58db316c59310
czasg <czasg0.0>
2021-12-11 14:55:04 +0800

    feat: update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,43 +12,49 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   const initScript = () => {
       var
-          words = ['女票', '涨薪', '健康', '快乐', '大厂', '长胖', '锻炼', '猫咪', '帝豪', '自信', '存钱', '勇敢'],
+          words = ['理财', '涨薪', '健康', '快乐', '大厂', '长胖', '锻炼', '猫咪', '奔驰', '自信', '存钱', '勇敢'],
           bg = document.querySelector('#bg'),
           ani = document.querySelector('#ani'),
           ctx_bg = bg.getContext('2d'),
-          ctx_ani = ani.getContext('2d');
-      bg.height = ani.height = 400;
-      bg.width = ani.width = 400;
+          ctx_ani = ani.getContext('2d'),
+          multiple = 400/260;
+      bg.height = ani.height = 260*multiple;
+      bg.width = ani.width = 260*multiple;
+      ctx_bg.strokeStyle = "rgba(255,255,255)";
+      ctx_ani.strokeStyle = "rgba(255,255,255)";
+      ctx_ani.fillStyle = "rgba(255,255,255)";
+      ctx_bg.lineWidth = 5;
+      ctx_ani.font = '16px Arial';
       /* 两个圈 */
       ctx_bg.beginPath();
-      ctx_bg.translate(200, 200);
-      ctx_bg.moveTo(80, 0);
-      ctx_bg.arc(0, 0, 80, 0, Math.PI * 2);
-      ctx_bg.moveTo(115, 0);
-      ctx_bg.arc(0, 0, 115, 0, Math.PI * 2);
+      ctx_bg.translate(130*multiple, 130*multiple);
+      ctx_bg.moveTo(84*multiple, 0);
+      ctx_bg.arc(0, 0, 84*multiple, 0, Math.PI * 2);
+      ctx_bg.moveTo(115*multiple, 0);
+      ctx_bg.arc(0, 0, 115*multiple, 0, Math.PI * 2);
       ctx_bg.stroke();
       /* 六芒星 */
       ctx_bg.beginPath();
-      ctx_bg.moveTo(-69, 40);
-      ctx_bg.lineTo(69, 40);
-      ctx_bg.lineTo(0, -80);
-      ctx_bg.lineTo(-69, 40);
-      ctx_bg.moveTo(-69, -40);
-      ctx_bg.lineTo(69, -40);
-      ctx_bg.lineTo(0, 80);
-      ctx_bg.lineTo(-69, -40);
+      ctx_bg.moveTo(-69*multiple, 40*multiple);
+      ctx_bg.lineTo(69*multiple, 40*multiple);
+      ctx_bg.lineTo(0, -80*multiple);
+      ctx_bg.lineTo(-69*multiple, 40*multiple);
+      ctx_bg.moveTo(-69*multiple, -40*multiple);
+      ctx_bg.lineTo(69*multiple, -40*multiple);
+      ctx_bg.lineTo(0, 80*multiple);
+      ctx_bg.lineTo(-69*multiple, -40*multiple);
       ctx_bg.stroke();
       /* 两个红三角 */
       ctx_bg.beginPath();
       ctx_bg.fillStyle = 'red';
-      ctx_bg.moveTo(0, -125);
-      ctx_bg.lineTo(-5, -115);
-      ctx_bg.lineTo(5, -115);
-      ctx_bg.lineTo(0, -125);
-      ctx_bg.moveTo(-125, 0);
-      ctx_bg.lineTo(-115, 5);
-      ctx_bg.lineTo(-115, -5);
-      ctx_bg.lineTo(-125, 0);
+      ctx_bg.moveTo(0, -125*multiple);
+      ctx_bg.lineTo(-5*multiple, -117*multiple);
+      ctx_bg.lineTo(5*multiple, -117*multiple);
+      ctx_bg.lineTo(0, -125*multiple);
+      ctx_bg.moveTo(-125*multiple, 0);
+      ctx_bg.lineTo(-117*multiple, 5*multiple);
+      ctx_bg.lineTo(-117*multiple, -5*multiple);
+      ctx_bg.lineTo(-125*multiple, 0);
       ctx_bg.fill();
 
       /* 生成器 */
@@ -70,16 +76,16 @@ function HomepageHeader() {
           loop2000 = loop(2000);
 
       var point60 = new Path2D();
-      point60.moveTo(120, 0);
-      point60.lineTo(125, 0);
+      point60.moveTo(120*multiple, 0);
+      point60.lineTo(125*multiple, 0);
 
       (function clockDrawing() {
           var value = loop2000.next().value;
 
-          ctx_ani.clearRect(0, 0, 400, 400);
+          ctx_ani.clearRect(0, 0, 260*multiple, 260*multiple);
 
           ctx_ani.save();
-          ctx_ani.translate(200, 200);
+          ctx_ani.translate(130*multiple, 130*multiple);
 
           ctx_ani.save();  // 60个点
           ctx_ani.rotate(value * (Math.PI / 500));
@@ -94,7 +100,7 @@ function HomepageHeader() {
           words.forEach(word => {
               ctx_ani.rotate(Math.PI / 6);
               ctx_ani.save();
-              ctx_ani.translate(85, 0);
+              ctx_ani.translate(90*multiple, 0);
               ctx_ani.rotate(word_rotate);
               ctx_ani.fillText(word, 5, 0);
               ctx_ani.restore();
@@ -107,7 +113,7 @@ function HomepageHeader() {
   };
   useEffect(() => {initScript()}, []);
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--dark', styles.heroBanner)}>
       <div className="container">
         <div className="row">
           <div className={clsx('col col--6')}>
