c5b004c885b3815ca54c3c6b66586c0a91af79ea
czasg <czasg0.0>
2021-12-12 20:01:29 +0800

    feat: upload blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,7 +136,7 @@ const config = {
         //     ],
         //   },
         // ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Czasg's Site.`,
       },
       prism: {
         theme: lightCodeTheme,
